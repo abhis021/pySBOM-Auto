693e323773f1078e63da4d8adb4efff1e41540ca
abhis021 <abhisheku722@gmail.com>
2025-05-22 11:01:19 +0530

    added license headers

--- report_generator.py
+++ report_generator.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2025 Your Name <your.email@example.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 from weasyprint import HTML
 
 def generate_report(cve_data: dict, output_dir='reports', as_pdf=False):
--- sbom_parser.py
+++ sbom_parser.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2025 Your Name <your.email@example.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 from spdx.parsers.jsonparser import Parser as SPDXParser
 from spdx.parsers.loggers import StandardLogger
 from spdx.document import Document
